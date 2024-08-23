382bf830b15c8f6866835ca424b80bb619d53e9d
LazyTurtle6661 <luukmoller@gmail.com>
2024-08-23 11:06:38 +0200

    Fixed template Created pages

--- 02_Web_template_CSS_Tricks/index.js
+++ 02_Web_template_CSS_Tricks/index.js
@@ -1,17 +1,33 @@
 
 
-let currentPage = 4
+let currentPage = 1
+let menuNumber = 1
+
 let pages // Array med alle elementer 
+let menuItems // Array med alle menu punkterne
+
 let colors = ['red', 'green', 'blue', 'lightgreen', 'orange', 'lightcyan', 'lightgrey', 'lightblue']
 
 function setup(){
     console.log("P5.js er loaded")
 
     select('#page' + currentPage).addClass('visible')
+    select('#menu' + menuNumber).addClass('active')
 
     pages = selectAll('.page')
+    menuItems = selectAll('.menuitem')
+
+    for( m of menuItems ){
+        m.mousePressed(function(e){
+            //e.target er selve html div'en
+            console.log(e.target)
+            // Slice -1 henter det sidste bogstav i en string
+            let nr = e.target.id.slice(-1)
+            shiftPage(nr)
+
+        })
+    }
     // Nu kan man se at pages er blevet til en liste med alle class = page ting //
-    //Hallo?
     console.log(pages.length)
 
     // Lav en masser diver, vi kommer ind i page 3
@@ -43,6 +59,11 @@ function shiftPage(num){
     select("#page" + currentPage).removeClass('visible')
     currentPage = num
     select("#page" + currentPage).addClass('visible')
+
+
+    select("#menu" + menuNumber).removeClass('active')
+    menuNumber = num
+    select("#menu" + menuNumber).addClass('active')
 
 }
 
